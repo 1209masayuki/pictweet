63e663ab86428662d970edcd463402ccd5c71671
1209masayuki <xkiddo22@yahoo.co.jp>
2020-07-22 20:58:13 +0900

    結合テストコード 新規登録がうまくいかないとき

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -30,12 +30,23 @@ RSpec.describe 'ユーザー新規登録' , type: :system do
   end
   context 'ユーザー新規登録ができないとき' do
     it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
-      # トップページに移動する
+      #トップページに移動する
+      visit root_path
       # トップページにサインアップページへ遷移するボタンがある
+      expect(page).to have_content('新規登録')
       # 新規登録ページへ移動する
+      visit new_user_registration_path
       # ユーザー情報を入力する
+      fill_in 'Nickname', with: ""
+      fill_in 'Email', with: ""
+      fill_in 'Password', with: ""
+      fill_in 'Password confirmation', with: ""
       # サインアップボタンを押してもユーザーモデルのカウントは上がらない
+      expect{
+      find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
       # 新規登録ページへ戻される
+      expect(current_path).to eq "/users"
     end
   end
 end
