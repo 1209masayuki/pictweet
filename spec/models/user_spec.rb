a90ad0a622a0ffde51badfebeccc4ec384a48229
1209masayuki <xkiddo22@yahoo.co.jp>
2020-07-22 12:20:54 +0900

    モデルのテストコード second

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,9 @@ describe User, type: :model do
       expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
     it "emailが空だと登録できない" do
+      user = User.new(nickname: "abe", email: "", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank")
     end
   end
 end
